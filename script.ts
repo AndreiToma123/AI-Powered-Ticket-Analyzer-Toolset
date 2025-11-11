d64a94603776ccb9865da8b6daf33e1f97e3d14c
AndreiToma123 <118735556+AndreiToma123@users.noreply.github.com>
2025-11-11 19:07:29 +0200

    database updates

--- script.ts
+++ script.ts
@@ -2,3 +2,19 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+async function main() {
+    const ticket = await prisma.ticket.create({
+        data: {name: "John Doe", requestDate: "2023-01-01", email: "W6Wg8@example.com", category: "Category A", urgency: "Urgent", status: "Pending"
+        },
+    });
+
+    console.log(ticket);
+}
+
+main()
+.catch((e) => {
+    console.error(e.message);
+})
+.finally(async () => {
+    await prisma.$disconnect();
+})

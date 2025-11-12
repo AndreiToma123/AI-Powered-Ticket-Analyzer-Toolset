9d54d400b4dfcd2aa4610f0b37c2725866ecc9a0
AndreiToma123 <118735556+AndreiToma123@users.noreply.github.com>
2025-11-12 15:48:24 +0200

    Update to database

--- script.ts
+++ script.ts
@@ -5,7 +5,12 @@ const prisma = new PrismaClient();
 async function main(){
 const ticket = await prisma.ticket.create({
     data: {
-        name: "Ticket 1",
+        name: "John Doe",
+        requestDate: "2023-01-01",
+        email: "john@example.com",
+        category: "Category A",
+        urgency: "Urgent",
+        status: "Pending"
     }
 })
 console.log(ticket)

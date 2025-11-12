861867e84446c3bb289de688e1e17d4cb3d49856
AndreiToma123 <118735556+AndreiToma123@users.noreply.github.com>
2025-11-12 10:40:45 +0200

    Replaced all files

--- script.ts
+++ script.ts
@@ -1,20 +1,20 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-    const ticket = await prisma.ticket.create({
-        data: {name: "John Doe", requestDate: "2023-01-01", email: "W6Wg8@example.com", category: "Category A", urgency: "Urgent", status: "Pending"
-        },
-    });
-
-    console.log(ticket);
+async function main(){
+const ticket = await prisma.ticket.create({
+    data: {
+        name: "Ticket 1",
+    }
+})
+console.log(ticket)
 }
 
 main()
-.catch((e) => {
-    console.error(e.message);
+.catch(e => {
+    console.error(e.message)
 })
 .finally(async () => {
-    await prisma.$disconnect();
+    await prisma.$disconnect()
 })
